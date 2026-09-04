78c6177befd3d49abe8b264016a5ce509abddc8f
KelvinLinus07 <kelvinlinus1610@gmail.com>
2026-09-04 07:32:13 +0530

    mongo connected

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,24 @@
 const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
+
 const app = express();
-const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Food Ordering API is running!"
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
